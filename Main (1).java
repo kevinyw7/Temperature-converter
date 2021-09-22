8ae4c403c9b181779e074c5a2331309661de590e
kevinyw7 <91022707+kevinyw7@users.noreply.github.com>
2021-09-21 21:01:49 -0500

    Update
    added feature where user can do multiple temperature conversions without restarting
    fixed bug involving integer division
    formatting/organization

--- Main (1).java
+++ Main (1).java
@@ -4,34 +4,53 @@ import java.lang.*;
 class Main{
 public static void main(String[] args) {
 
-// ask the user what is there starting temperature
-Scanner Temp = new Scanner(System.in);
-System.out.println("what temperature are you starting with? Type C for Celsius and F for Fahrenheit");
+//ask the user how many conversions they wish to make
+Scanner start = new Scanner(System.in);
+System.out.println("how many conversions do you need to make? (please answer in a postivie integer)");
+int times = start.nextInt();
 
-String CorF = Temp.nextLine();
-
-
-// ask the user how many degrees
-Scanner number = new Scanner(System.in);
-System.out.println("how many degrees?");
-
-Double degrees = number.nextDouble();
-
-
-// convert from celsius to fahrenheit
-if(CorF.equals("C") || CorF.equals("c"))
+//loop code for how many conversions need to be made
+for( int i = 0; i < times; i++)
 {
-	System.out.println( degrees * (9/5) + 32);
-	System.out.println( "degrees Fahrenheit");
-}
+
+
+  // ask the user what is there starting temperature
+  Scanner Temp = new Scanner(System.in);
+  System.out.println("what temperature are you starting with? Type C for Celsius and F for Fahrenheit");
+
+  String CorF = Temp.nextLine();
+
+
+  // ask the user how many degrees
+  Scanner number = new Scanner(System.in);
+  System.out.println("how many degrees?");
+
+  Double degrees = number.nextDouble();
+
+  System.out.println();
+
+  // convert from celsius to fahrenheit
+  if(CorF.equals("C") || CorF.equals("c"))  
+  {
+    	System.out.println( degrees * (9.0/5) + 32);
+	    System.out.println( "degrees Fahrenheit");
+  }
 
 // convert from fahrenheit to celsius
 if(CorF.equals("F") || CorF.equals("f"))
-{
-	System.out.println( (degrees + 32) * (9/5) );
-	System.out.println( "degrees Celsius");
+  {
+	  System.out.println( (degrees - 32) * (9.0/5) );
+	  System.out.println( "degrees Celsius");
+  }
+
+  }
+
+//end of loop
 }
 
+//end of main class
 }
-}
+
+
+
 
